46277652e08fac7c2db879895c1a68e474ff4ef4
Yashraj Singh Parihar <128150523+Yashrajsinghparihar@users.noreply.github.com>
2023-11-13 12:08:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+let nameInput = document.querySelector(".PropertyName")
+let priceInput = document.querySelector(".Price");
+let locationInput = document.querySelector(".Location");
+
+let createBtn = document.querySelector(".create");
+let deleteBtn = document.querySelector(".delete");
+
+let mainCont = document.querySelector(".mainContainer");
+
+let flag = false;
+
+window.addEventListener("load",function(){
+    if(this.localStorage.getItem("obj")){
+
+        let obj = JSON.parse(localStorage.getItem("obj"));
+        let arr = obj.arr;
+
+        
+
+        for(let i=0;i<arr.length-3;i+=3){
+            let ele = this.document.createElement("div");
+            ele.innerHTML = `<div  class="ticket" >
+
+            <h2 >${arr[i]} </h2>
+            <p  >${arr[i+1]}</p>
+            <p  >${arr[i+2]}</p>
+          
+           </div>`
+          
+              ele.addEventListener("click", function () {
+                  if (flag == true) {
+                      mainCont.removeChild(ele);
+                  }
+              })
+          
+              mainCont.appendChild(ele)
+        }
+    }
+})
+
+deleteBtn.addEventListener("click", function () {
+    flag = !flag;
+
+    if (flag == true) {
+        deleteBtn.style.color = "red"
+    } else {
+        deleteBtn.style.color = "black"
+    }
+})
+
+createBtn.addEventListener("click", ticketCreator)
+
+function ticketCreator() {
+    let ele = document.createElement("div");
+
+    let ticketDetails = localStorage.getItem("obj");
+
+    console.log(ticketDetails);
+
+    if (ticketDetails == undefined) {
+        let obj = { arr: [nameInput.value,priceInput.value, locationInput.value]}
+        localStorage.setItem("obj",JSON.stringify(obj));
+    }
+    else{
+        let obj = JSON.parse(ticketDetails);
+        let arr = obj.arr;
+        let narr = [...arr , nameInput.value,priceInput.value, locationInput.value]
+        let newobj = {arr:narr};
+        localStorage.setItem("obj",JSON.stringify(newobj));
+    }
+
+
+    ele.innerHTML = `<div  class="ticket" >
+
+  <h2 >${nameInput.value} </h2>
+  <p  >${priceInput.value}</p>
+  <p  >${locationInput.value}</p>
+
+ </div>`
+
+    ele.addEventListener("click", function () {
+        if (flag == true) {
+            mainCont.removeChild(ele);
+        }
+    })
+
+    mainCont.appendChild(ele)
+}
